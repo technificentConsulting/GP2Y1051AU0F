5dea11455d1de0e9cd293ba4d3021fe7911574bb
Jheng-lin (Johnny) Jiang <nick2nkite@hotmail.com>
2019-02-11 12:33:32 +0800

    some minor changes

--- airq/airq.py
+++ airq/airq.py
@@ -7,7 +7,7 @@ import serial
 class AIRQ():
     """
     This program will read the data from Sharp GP2Y1051AU0F dust density sensor.
-    The device will keep sending 7 bytes serial data every 100ms.
+    The device will keep sending 7 bytes serial data every 10ms.
     For example:
     AA 00 02 00 61 63 FF AA 00 02 00 61 63 FF AA 00 02 00 61 63
     FF AA 00 02 00 61 63 FF AA 00 02 00 61 63 FF ... ...
@@ -17,8 +17,7 @@ class AIRQ():
 
     Where represents AA represents the begining byte, FF represents the ending byte.
 
-    Second byte 00 is the Vout high
-    Third byte 02 is the Vout low
+    Second byte 00 is the Vout high, third byte 02 is the Vout low
     """
     def __init__(self, ser):
         self.ser = ser
@@ -47,6 +46,9 @@ class AIRQ():
         return formatted_chunk
 
     def num_format(self, num_hex):
+        """
+        Transform Hex value to decimal
+        """
         num_int = int(num_hex, 16)
         return float(format(num_int, '.10f'))
 
@@ -58,7 +60,7 @@ class AIRQ():
 
         # Caculate the Vout
         Vout = ((vout_h * 256) + vout_l) * 5 / 1024
-        vout = round(Vout, 3)
+        vout = round(Vout, 4)
         return vout
 
     def get_k(self, vout):
@@ -66,33 +68,33 @@ class AIRQ():
         """
         K represents a specific coefficient for the GP2Y1051AU0F dust sensor.
         The value of K will vary from the given value of Vout
-        The conditions are searched from the internet, so it could be wrong.
+        The conditions are searched from the internet, so it might be not correct.
         """
         if vout < 0.046:
             return 200
-        if vout < 0.049:
+        elif vout < 0.049:
             return 400
-        if vout < 0.052:
+        elif vout < 0.052:
             return 600
-        if vout < 0.055:
+        elif vout < 0.055:
             return 750
-        if vout < 0.059:
+        elif vout < 0.059:
             return 900
-        if vout < 0.065:
+        elif vout < 0.065:
             return 1000
-        if vout < 0.071:
+        elif vout < 0.071:
             return 1250
-        if vout < 0.076:
+        elif vout < 0.076:
             return 1400
-        if vout < 0.081:
+        elif vout < 0.081:
             return 1700
-        if vout < 0.086:
+        elif vout < 0.086:
             return 1800
-        if vout < 0.091:
+        elif vout < 0.091:
             return 1900
-        if vout < 0.101:
+        elif vout < 0.101:
             return 2000
-        if vout < 0.111:
+        elif vout < 0.111:
             return 2200
         else:
             return 3000
@@ -109,11 +111,12 @@ class AIRQ():
 if __name__ == '__main__':
 
     def show(aq):
-
+        chunk = aq.get_serial_chunk()
         vout = aq.get_vout()
+        k = aq.get_k(vout)
         density = aq.get_density()
 
-        sys.stdout.write("[  Vout: %s V     |    Dust density: %s ug/m3 ] \r" % (vout, density))
+        sys.stdout.write("[ %s | K: %s | Vout: %s V | Dust density: %s ug/m3 ] \r" % ( chunk, k, vout, density))
         sys.stdout.flush()
         time.sleep(1)
 
